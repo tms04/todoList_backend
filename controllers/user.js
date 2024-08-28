97cc3af71f919e7ca061bb4b6d4a326c7cf8f18d
tms04 <138638288+tms04@users.noreply.github.com>
2024-08-28 17:22:16 +0000

    commit

--- controllers/user.js
+++ controllers/user.js
@@ -54,8 +54,8 @@ export const newUser=async (req,res)=>{
 export const logout=(req,res)=>{
     res.status(200).cookie("token","",{
         expires:new Date(Date.now()),
-        sameSite:process.env.NODE_ENV==="Development"?"lax":"none",
-        secure:process.env.NODE_ENV==="Development"? false:true,
+        sameSite:"none",
+        secure:true,
     }).json({
         success:true,
         message:"Logged out successfully"
